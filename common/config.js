2117b5cea15dea3ba407e8a542eb6d5cd4b0e8fb
fy <fyonecon@gamil.com>
2026-06-15 10:18:19 +0800

    v1.1.3

--- common/config.js
+++ common/config.js
@@ -1,5 +1,5 @@
 const config = {
-    app_name: "tabPreHome", // 应用名
+    app_name: "tabPureHome", // 应用名
     app_class: "tab_purehome_", // 必须是字母、数字、下划线、非数值开头
     app_version: "1.1.3", // 版本号 1.0.0
     console_debug: false, // 显示console日志。false true
