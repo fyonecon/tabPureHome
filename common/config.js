a19f6b172d7955bfb4df9d0871630e9bfa939adb
fy <fyonecon@gamil.com>
2026-07-29 01:25:58 +0800

    update

--- common/config.js
+++ common/config.js
@@ -1,7 +1,7 @@
 const config = {
     app_name: "tabPureHome", // 应用名
     app_class: "tab_purehome_", // 必须是字母、数字、下划线、非数值开头
-    app_version: "1.2.3", // 版本号 1.0.0
+    app_version: "1.2.4", // 版本号 1.0.0
     console_debug: false, // 显示console日志。false true
     storage_type: "sync", // 可选: "sync" | "local"，默认 sync
     default_new_tab_href: "https://datathink.top/purehome", // 默认网址
